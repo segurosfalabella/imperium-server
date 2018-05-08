d9d504c39e6495a9f109e89a66391929a4014514
Sebastian Diaz <seadiaz@gmail.com>
2018-05-08 17:31:04 -0300

    fix(dispatcher): wrap dispatch to use job struct instead string for message serialization

--- dispatcher/dispatcher.go
+++ dispatcher/dispatcher.go
@@ -1,16 +1,36 @@
 package dispatcher
 
 import (
+	"encoding/json"
 	"log"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"github.com/segurosfalabella/imperium-server/connection"
 )
 
+// Job struct
+type Job struct {
+	ID       string
+	Command  string
+	Response string
+	ExitCode int
+}
+
+// ToJSON method
+func (job *Job) ToJSON() string {
+	binary, _ := json.Marshal(job)
+	return string(binary)
+}
+
 // Dispatch ..
 func Dispatch(conn connection.WsConn, message string) {
 	for {
-		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		job := Job{
+			ID:      uuid.New().String(),
+			Command: "health",
+		}
+		err := conn.WriteMessage(websocket.TextMessage, []byte(job.ToJSON()))
 		if err != nil {
 			log.Println("There was an error sending task:", err)
 			return
